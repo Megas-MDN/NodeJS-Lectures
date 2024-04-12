52558d5da181d897e3c3e19cf281a5631579986e
megas-mdn <mdn.melqui@gmail.com>
2024-04-12 17:12:29 -0300

    feat: Data files

---
+++ sources/data/products.js
@@ -0,0 +1,34 @@
+const productsList = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 15,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 21,
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 31,
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    price: 44,
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    price: 9.9,
+  },
+];
+
+module.exports = productsList;
---
+++ sources/data/students.js
@@ -0,0 +1,29 @@
+const studentsList = [
+  {
+    id: "1",
+    name: "John",
+    grades: [10, 9, 5],
+  },
+  {
+    id: "2",
+    name: "Jane",
+    grades: [5, 10, 0],
+  },
+  {
+    id: "3",
+    name: "Bob",
+    grades: [7, 7, 6.5],
+  },
+  {
+    id: "4",
+    name: "Alice",
+    grades: [5.5, 10, 7],
+  },
+  {
+    id: "5",
+    name: "Mark",
+    grades: [8, 8.5, 9.8],
+  },
+];
+
+module.exports = studentsList;
