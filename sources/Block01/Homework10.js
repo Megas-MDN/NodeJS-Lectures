028fff7f5666c62c06e1b227ac7d7c1917243673
megas-mdn <mdn.melqui@gmail.com>
2024-05-27 15:26:11 -0300

    fix get funtion and add docs

--- sources/Block01/Homework10.js
+++ sources/Block01/Homework10.js
@@ -14,13 +14,14 @@ const fnToStr = (item) => {
 };
 
 class customHash {
-  #toStrFn;
-  #table;
+  #toStrFn; // function to convert item to string
+  #table; // hash table it self
   constructor(toStrFn = fnToStr) {
     this.#toStrFn = toStrFn;
     this.#table = {};
   }
 
+  // hash function get the key string and return the hash number
   #loseloseHash(key) {
     if (typeof key === "number") return key;
     const str = this.#toStrFn(key);
@@ -33,14 +34,16 @@ class customHash {
     return hash;
   }
 
+  // put the value in the hash table
   put(key, value) {
     if (key === null || key === undefined) return false;
     if (value === null || value === undefined) return false;
-    const hashKey = this.#loseloseHash(key);
+    const hashKey = this.#loseloseHash(key); // get the hash from key
     this.#table[hashKey] = value;
     return hashKey;
   }
 
+  // insert the value in the hash table
   insert(key, value) {
     if (this.#loseloseHash(key) in this.#table) {
       if (Array.isArray(this.#table[this.#loseloseHash(key)])) {
@@ -51,6 +54,7 @@ class customHash {
     return this.put(key, value);
   }
 
+  // delete the value in the hash table
   delete(key) {
     if (!(this.#loseloseHash(key) in this.#table)) return null;
     const item = this.#table[this.#loseloseHash(key)];
@@ -58,14 +62,17 @@ class customHash {
     return item;
   }
 
+  // get the value in the hash table
   get(key) {
-    return this.#table[this.#toStrFn(key)];
+    return this.#table[this.#loseloseHash(key)];
   }
 
+  // get the table
   get table() {
     return this.#table;
   }
 
+  // log the table
   log() {
     console.table(this.#table);
   }
@@ -73,13 +80,20 @@ class customHash {
 
 (() => {
   const table = new customHash();
-  table.insert("key", "test@test.com");
-  table.insert("abc", "other@test.com");
-  table.insert("key", "xxxtest@xxx.com");
-  table.insert("key", "000teste@101.com");
-  console.log(table.table, "<---");
-  table.delete("key");
-  console.log(table.table, "<---");
+
+  table.insert("abc", "value1");
+  table.insert("bca", "value2");
+
+  console.log(table.get("abc")); // "value1"
+
+  console.log(table.get("bca")); // "value2"
+
+  table.delete("abc");
+  console.log(table.get("abc")); // undefined
+  console.log(table.get("bca")); // "value2"
+
+  table.delete("bca");
+  console.log(table.get("bca")); // undefined
 })();
 
 module.exports = customHash;
